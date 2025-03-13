2a0bccf5970bd1fc56479431e53a3eb624eaa3fb
bhuang <biaohwang6@gmail.com>
2025-03-13 14:03:41 +0000

    test

---
+++ terraform/ec2/variables.tf
@@ -0,0 +1,12 @@
+# 你可以根据需要添加更多变量
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
