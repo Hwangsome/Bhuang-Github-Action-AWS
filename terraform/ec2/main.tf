d5c0c6a4506a3fdfde6ed2b1d88fd68988373fd1
bhuang <biaohwang6@gmail.com>
2025-03-13 14:58:31 +0000

    test

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -15,46 +15,46 @@ data "aws_subnets" "default" {
   }
 }
 
-# module "ec2_instance" {
-#   source  = "terraform-aws-modules/ec2-instance/aws"
-#   version = "~> 5.0"
-#
-#   name = "my-ec2-instance"
-#
-#   instance_type          = var.instance_type
-#   ami                    = "ami-0735c191cf914754d"  # Amazon Linux 2 in us-west-2
-#   monitoring             = true
-#   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
-#   subnet_id              = element(data.aws_subnets.default.ids, 0)
-#
-#   tags = {
-#     Terraform   = "true"
-#     Environment = "dev"
-#   }
-# }
-#
-# # 创建安全组
-# resource "aws_security_group" "allow_ssh" {
-#   name        = "allow_ssh"
-#   description = "Allow SSH inbound traffic"
-#   vpc_id      = data.aws_vpc.default.id
-#
-#   ingress {
-#     description = "SSH from anywhere"
-#     from_port   = 22
-#     to_port     = 22
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-#
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-#
-#   tags = {
-#     Name = "allow_ssh"
-#   }
-# }
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 5.0"
+
+  name = "my-ec2-instance"
+
+  instance_type          = var.instance_type
+  ami                    = "ami-08b5b3a93ed654d19"  # Amazon Linux 2 in us-west-2
+  monitoring             = true
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  subnet_id              = element(data.aws_subnets.default.ids, 0)
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+# 创建安全组
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
